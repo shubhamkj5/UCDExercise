7a9a3917294e767ce70904e2fe3311386eb931ca
shubhamkj5 <shubhamkj2405@gmail.com>
2021-01-28 20:48:33 +0000

    Initialized a repo

---
+++ module1.py
@@ -0,0 +1,10 @@
+# Define shout with the parameter, word
+def shout(____):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    ____ = ____ + '!!!'
+
+    # Print shout_word
+    print(____)
+
+# Call shout with the string 'congratulations'
